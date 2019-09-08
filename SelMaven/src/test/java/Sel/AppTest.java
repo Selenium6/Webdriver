880726a5181ac15e039923f078e7d05feaa43a1a
vasudeva <vasu.584@gmail.com>
2019-09-08 11:57:30 +0530

    Complete Project

---
+++ SelMaven/src/test/java/Sel/AppTest.java
@@ -0,0 +1,19 @@
+package Sel;
+
+
+import org.testng.annotations.Test;
+
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+	@Test
+	public void Sample()
+	{
+		System.out.println("Selenium");
+	}
+}
